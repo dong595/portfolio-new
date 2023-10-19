6feb900f530a6055b7189630d4ac0e83a88b4182
dong959 <binz2028@gmail.com>
2023-10-19 19:46:35 +0700

    user interface

---
+++ src/Components/Home.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+
+const Home = () => {
+  return (
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
+      {/* container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-pink-600">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Nguyen Van Dong
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I'm a Frontend Developer.
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          Enthusiastic focused, highly motivated Intern Frontend Developer with
+          experience in building and maintaining web applications. Eager to
+          apply my experience with ReactJS to create high-quality software
+          products that contribute to the success of the company. I enjoy
+          working collaboratively with team and willingness to learn new
+          technologies. Seeking a dynamic and professional working environment.{" "}
+        </p>
+        <div>
+          <button className="flex items-center group gap-2 border-2 my-2 hover:bg-pink-600 hover:border-pink-600 rounded text-white  py-3 px-6">
+            View work{" "}
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/data/data.js
@@ -0,0 +1,27 @@
+import angular from "../assets/projects/angular-admin.png";
+import duan1 from "../assets/projects/duan1.png";
+import portfolio from "../assets/projects/portfolio.png";
+export const data = [
+  {
+    id: 1,
+    name: "Angular Application",
+    image: angular,
+    github: "https://github.com/dong595/Product-Manager",
+    live: "",
+  },
+  {
+    id: 2,
+    name: "Project 1 Application",
+    image: duan1,
+    github:
+      "https://github.com/Phamtuan19/PRO1014_WE18103_Nhom_6/tree/dongnvph28919",
+    live: "",
+  },
+  {
+    id: 3,
+    name: "Porfolio using Javascrip Vanila Application",
+    image: portfolio,
+    github: "https://github.com/dong595/portfolio",
+    live: "https://portfolio-git-main-dong595.vercel.app/?fbclid=IwAR2vdlfBtk61vAnLTW6_yDqnUQQUbgSkXSgKJ5yH4dhuJvYZ4mTbDzeKlpE",
+  },
+];
