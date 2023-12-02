5d420e0f13f4c58f39da42eb65d9dc583b4648a5
dong959 <binz2028@gmail.com>
2023-12-03 01:25:11 +0700

    update skill

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -7,6 +7,7 @@ import Node from "../assets/images/node.png";
 import GitHub from "../assets/images/github.png";
 import Tailwind from "../assets/images/tailwind.png";
 import Mongo from "../assets/images/mongo.png";
+import Angular from "../assets/images/angular.png";
 const Skills = () => {
   return (
     <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
@@ -16,7 +17,7 @@ const Skills = () => {
           <p className="text-4xl font-bold inline border-b-4 border-pink-600 ">
             Skills
           </p>
-          <p className="py-4">// These are the technologies I've worked with</p>
+          <p className="py-4"> These are the technologies I've worked with</p>
         </div>
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
@@ -35,6 +36,10 @@ const Skills = () => {
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={ReactImg} alt="HTML icon" />
             <p className="my-4">REACT</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Angular} alt="HTML icon" />
+            <p className="my-4">Angular</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={GitHub} alt="HTML icon" />
